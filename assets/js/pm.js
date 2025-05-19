757bcffab4548923a7b69e7a2aded803124b86a2
justfiz <kurniawanhafizzz@gmail.com>
2025-05-19 23:15:35 +0700

    first commit ygy, semoga no bug

---
+++ assets/js/pm.js
@@ -0,0 +1,6 @@
+    AOS.init({ once: true, offset: 100, duration: 800, easing: 'ease-in-out' });
+
+    function openImage(img) {
+      document.getElementById("modal-img").src = img.src;
+      document.getElementById("image-modal").style.display = "flex";
+    }
